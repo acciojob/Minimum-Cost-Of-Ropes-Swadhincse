8517e853fb999744d70cf3df4608610dfe377837
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-02-28 00:06:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,11 +12,9 @@ function minCostToFormRope(a) {
 
     while(arr.length > 1){
         var res = Number(arr[0]) + Number(arr[1]);
-        arr.splish(res);
-        cost += res;
-        arr.shift();
-        arr.shift();
-
+        arr.splish(0,2);
+        arr.push(res);
+		cost += res;
         arr.sort(function (a,b) {return a-b});
     }
     document.getElementById("result").textcontent=cost;
